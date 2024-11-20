4a238a4ac9cfe0d5dbf76a03279c160b0ac26ef0
Ramdani2510 <ramdanitrsn@gmail.com>
2024-11-20 10:26:38 +0700

    add few migrate transaction

--- database/migrations/2024_11_06_073017_create_transactions_table.php
+++ database/migrations/2024_11_06_073017_create_transactions_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('cart_id')->constrained('carts');
+            $table->integer('total_price');
+            $table->string('status');
             $table->timestamps();
         });
     }
